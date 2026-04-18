642de887f79e296d6bf868e5b528c6fd8f8a9f66
leejaeman2 <lividious2@naver.com>
2026-04-18 19:00:42 +0900

    add notice id router

--- notice/router.py
+++ notice/router.py
@@ -1,4 +1,5 @@
 from ninja import Router
+from django.shortcuts import get_object_or_404
 from notice.models import Notice
 from notice.schema import NoticeOutputSchema
 
@@ -18,3 +19,15 @@ def get_notices(request):
     }
     for n in notices
   ]
+
+@router.get('{int:notice_id}/', response=NoticeOutputSchema)
+def get_notice(request, notice_id: int):
+  n = get_object_or_404(Notice, id=notice_id, is_deleted=False)
+  return {
+    'id': n.id,
+    'author': n.author,
+    'type': n.type,
+    'title': n.title,
+    'content': n.content,
+    'created_at': str(n.created_at),
+  }
